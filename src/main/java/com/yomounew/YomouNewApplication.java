6546bd79a1c77b1be213d6528e3dec6e030bb876
Issei0813star <issei0813star.dev@gmail.com>
2024-09-14 16:36:10 +0900

    initial commit

---
+++ src/main/java/com/yomounew/YomouNewApplication.java
@@ -0,0 +1,13 @@
+package com.yomounew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YomouNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YomouNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yomounew/YomouNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yomounew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YomouNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
